c1badbe5279e1a92165cbfe1d8fb13f8b6afed47
AMANKUMAR1020 <amankumarchouhan1020@gmail.com>
2026-06-06 21:09:32 +0530

    update Client.java

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,5 @@ import structureDesignPattern.AdapterPattern.Client;
 
 public static void main(String[] args) {
     Client client = new Client();
+    client.execute();
 }
--- src/structureDesignPattern/AdapterPattern/Client.java
+++ src/structureDesignPattern/AdapterPattern/Client.java
@@ -19,13 +19,36 @@
 //}
 
 
+//package structureDesignPattern.AdapterPattern;
+//
+//public class Client {
+//
+//    public Client() {
+//        System.out.println("Constructor is created!");
+//
+//        String to = "customer@codingminutes.com";
+//        String subject = "Order Confirmation";
+//        String body = "Your order has been received!";
+//
+//        NotificationService emailService = new EmailNotificationService();
+//        emailService.send(to, subject, body);
+//
+//        NotificationService emailServiceUsingSendGrid =
+//                new SendGridAdapter(new SendGridService());
+//
+//        emailServiceUsingSendGrid.send(to, subject, body);
+//    }
+//}
+
+
+
 package structureDesignPattern.AdapterPattern;
 
 public class Client {
 
-    public Client() {
-        System.out.println("Constructor is created!");
+    public Client() {}
 
+    public void execute() {
         String to = "customer@codingminutes.com";
         String subject = "Order Confirmation";
         String body = "Your order has been received!";
